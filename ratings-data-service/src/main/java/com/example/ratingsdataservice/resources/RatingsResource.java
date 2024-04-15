2bb116a347203d169893aa530abe28296e0a4a9e
Ian Perfitt <iancp@protonnmail.com>
2024-04-15 13:24:47 -0500

    updated movie ids to 100

--- ratings-data-service/src/main/java/com/example/ratingsdataservice/resources/RatingsResource.java
+++ ratings-data-service/src/main/java/com/example/ratingsdataservice/resources/RatingsResource.java
@@ -21,8 +21,8 @@ public class RatingsResource {
     @RequestMapping("users/{userId}")
     public UserRating getUserRating(@PathVariable("userId") String userId) {
         List<Rating> ratings = Arrays.asList(
-                new Rating("1234", 4),
-                new Rating("5678", 3)
+                new Rating("100", 4),
+                new Rating("100", 3)
         );
         UserRating userRating = new UserRating();
         userRating.setUserRating(ratings);
